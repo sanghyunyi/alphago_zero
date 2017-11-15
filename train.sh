2e6f72e71f2e081ba77fa1f7f4dc6d912960f347
sangyi92 <sangyi92@snu.ac.kr>
2017-11-15 20:26:31 +0900

    better training

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-screen -d -m python train_optimization.py
+screen -dm bash -c 'python train_optimization.py; exec sh'
 sleep 5
-screen -d -m python train_evaluator.py
-screen -d -m python train_self_play.py
+screen -dm bash -c 'python train_evaluator.py; exec sh'
+screen -dm bash -c 'python train_self_play.py; exec sh'
